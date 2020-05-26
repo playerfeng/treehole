66cec01edad63f7be3ecfbab84728babaefd6618
playerfeng <2835387047@qq.com>
2020-05-26 15:38:47 +0800

    first commit

---
+++ treehole/src/test/java/com/sdp3/treehole/TreeholeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdp3.treehole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeholeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
